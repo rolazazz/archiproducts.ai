e4234e11fcba3e9a531fbe26f2b2c2a08d78dd79
Roberto Lazazzera <lazazzera@edilportale.com>
2024-09-11 10:31:49 +0200

    get Embeddings only when specific fields are changing

--- py/pika/router.py
+++ py/pika/router.py
@@ -5,15 +5,19 @@ from aio_pika.abc import AbstractIncomingMessage
 
 from .tasks import test_task, product_changed_event_handler
 
+fieldsToWatch = set(['Name','ShortDescription','CoverImage','Attributes','Materials','Styles','Features','Designers'])
 
 async def message_router(
         message: AbstractIncomingMessage,
 ) -> None:
     async with message.process():
         body = json.loads(message.body.decode())
+        
         if body.get('type') == 'test_message':
             return test_task(body)
-        if 'urn:message:Edil.Shared.Contracts.Events:ProductChangedEvent' in body.get('messageType'):
+        
+        if 'urn:message:Edil.Shared.Contracts.Events:ProductChangedEvent' in body.get('messageType') \
+        	and set(body['message']['updatedFields']).intersection(fieldsToWatch):
             # return product_changed_event_handler(body)
             loop = asyncio.get_running_loop()
             await loop.run_in_executor(None, product_changed_event_handler, body)
